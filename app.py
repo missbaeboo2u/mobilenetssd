e1720276a81808376bd4ada6d60c8245c226aadc
missbaeboo2u <89238163+missbaeboo2u@users.noreply.github.com>
2021-11-11 11:18:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        if (msg == "เเพรวพราว") :
-           replyObj = TextSendMessage(text="สุดยอด")
+        if (msg == "สวัสดี") :
+           replyObj = TextSendMessage(text="ดีด้วย")
         else :
            replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
