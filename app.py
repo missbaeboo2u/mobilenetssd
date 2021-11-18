ef4d4b2f383ae46404f81fcd0c45c5ecef080535
missbaeboo2u <89238163+missbaeboo2u@users.noreply.github.com>
2021-11-18 10:51:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,16 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
            replyObj = TextSendMessage(text="ดีด้วย")
+           line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "เเพรวพราวจัง") :
            replyObj = TextSendMessage(text="ขอบคุณที่เเจ้งให้ทราบ")
+           line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "เธอเป็นอะไร่หร๊อ") :
            replyObj = TextSendMessage(text="ตัวร้อน")
+           line_bot_api.reply_message(rtoken, replyObj)
         else :
            replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+           line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
